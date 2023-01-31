08b673bf062da9d36769957462bc3c620e4e9fdd
“TAEWONgit config --global user.email “youbeen123@gmail.com <taewon@gimtaewon-ui-MacBookAir.local>
2023-01-31 15:02:06 +0900

    feat : 메뉴바 css 1차 완료

--- src/Components/Header/HeaderNav.jsx
+++ src/Components/Header/HeaderNav.jsx
@@ -4,13 +4,15 @@ import "./HeaderNav.scss";
 
 const HeaderNav = () => {
     return (
-        <div>
-            <Link to="/">차트</Link>
-            <Link to="/whook">Whook</Link>
-            <Link to="/event">이벤트</Link>
-            <Link to="/news">뉴스</Link>
-            <Link to="/store">스토어</Link>
-            <Link to="/charge">충전소</Link>
+        <div className='header_navbar'>
+            <div className='inner'>
+                <Link className='chart nav_item' to="/">차트</Link>
+                <Link className='whook nav_item' to="/whook">Whook</Link>
+                <Link className='event nav_item' to="/event">이벤트</Link>
+                <Link className='news nav_item' to="/news">뉴스</Link>
+                <Link className='store nav_item' to="/store">스토어</Link>
+                <Link className='charge nav_item' to="/charge">충전소</Link>
+            </div>
         </div>
     );
 };
